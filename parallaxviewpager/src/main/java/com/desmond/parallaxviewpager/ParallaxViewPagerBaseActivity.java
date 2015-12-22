4d0d3c804e760deaf0a514a8eec4b3903422a458
brucetoo <brucetoo14@gmail.com>
2015-12-22 10:12:51 +0800

    find a better way to listen scrollY of Listview

--- parallaxviewpager/src/main/java/com/desmond/parallaxviewpager/ParallaxViewPagerBaseActivity.java
+++ parallaxviewpager/src/main/java/com/desmond/parallaxviewpager/ParallaxViewPagerBaseActivity.java
@@ -3,10 +3,12 @@ package com.desmond.parallaxviewpager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ScrollView;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  * Created by desmond on 1/6/15.
@@ -31,6 +33,11 @@ public abstract class ParallaxViewPagerBaseActivity extends AppCompatActivity im
     protected abstract void scrollHeader(int scrollY);
     protected abstract void setupAdapter();
 
+    /**this method only works on listView with the same item height and without header
+     * look at this:
+     * http://stackoverflow.com/questions/10808387/android-getting-exact-scroll-position-in-listview?answertab=oldest#tab-top
+     * @deprecated use getScrollY replaced
+     * */
     protected int getScrollYOfListView(AbsListView view) {
         View child = view.getChildAt(0);
         if (child == null) {
@@ -48,6 +55,25 @@ public abstract class ParallaxViewPagerBaseActivity extends AppCompatActivity im
         return -top + firstVisiblePosition * child.getHeight() + headerHeight;
     }
 
+    private Dictionary<Integer, Integer> listViewItemHeights = new Hashtable<Integer, Integer>();
+
+    /**
+     * Use this method to replace getScrollYofListView will work fine in anytime
+     * Get listview top scroll height
+     * @param view
+     * @return
+     */
+    private int getScrollY(AbsListView view) {
+        View c = view.getChildAt(0);
+        int scrollY = -c.getTop();
+        listViewItemHeights.put(view.getFirstVisiblePosition(), c.getHeight());
+        for (int i = 0; i < view.getFirstVisiblePosition(); ++i) {
+            if (listViewItemHeights.get(i) != null)
+                scrollY += listViewItemHeights.get(i); //顶部消失的所有Item的高度
+        }
+        return scrollY;
+    }
+
     protected ParallaxViewPagerChangeListener getViewPagerChangeListener() {
         return new ParallaxViewPagerChangeListener(mViewPager, mAdapter, mHeader);
     }
@@ -59,7 +85,8 @@ public abstract class ParallaxViewPagerBaseActivity extends AppCompatActivity im
     public void onListViewScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
                                  int totalItemCount, int pagePosition) {
         if (mViewPager.getCurrentItem() == pagePosition) {
-            scrollHeader(getScrollYOfListView(view));
+//            scrollHeader(getScrollYOfListView(view));
+            scrollHeader(getScrollY(view));
         }
     }
 
